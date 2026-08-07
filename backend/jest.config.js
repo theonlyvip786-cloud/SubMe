881ff0e54b74071a24dddce07fa63cbc580d05c8
Rehan Husain <rehanhusain592@gmail.com>
2026-08-07 19:57:41 +0530

    Configure backend jest.config.js and add verified unit test suite

---
+++ backend/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/__tests__/**/*.test.js'],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
